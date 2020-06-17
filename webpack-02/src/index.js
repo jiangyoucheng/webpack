5fa961bbde12d3d3febfc8bdc4148189eff3ed96
jiangyoucheng <443656226@qq.com>
2020-06-17 16:43:56 +0800

    配置react打包环境

--- webpack-02/src/index.js
+++ webpack-02/src/index.js
@@ -2,5 +2,7 @@
 
 // axios.get('/api/info').then(res => console.log(res));
 
+import "./react.js";
+
 const arr = [new Promise(() => {}), new Promise(() => {})];
 arr.map(item => console.log(item));
